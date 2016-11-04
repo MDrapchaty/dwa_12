9064592b58d84a4dd54490bd70a46e729ee482d4
Matt Drapchaty <Drapchaty@comcast.net>
2016-11-04 01:20:39 -0600

    assign 3 complete!

--- url-shortener/src/server.js
+++ url-shortener/src/server.js
@@ -3,51 +3,12 @@
 var express = require('express');
 var body_parser = require('body-parser');
 var app = express();
-
-
+var logger = require('../lib/logger');
+/*
 const debug = require('debug')('my-namespace');  
 const name = 'my-app'  ;
 debug('booting %s', name);
-
-
-var winston = require('winston');
-winston.emitErrs = true;
-
-var logger = new winston.Logger({
-    transports: [
-        new winston.transports.File({
-            
-            level: 'debug',
-            filename: 'all-logs.log',
-            handleExceptions: true,
-            json: true,
-            maxsize: 5242880, //5MB
-            maxFiles: 5,
-            colorize: false
-
-        }),
-        new winston.transports.Console({
-            level: 'debug',
-            handleExceptions: true,
-            json: false,
-            colorize: true
-        })
-    ],
-    exitOnError: false
-});
-
-module.exports = logger;
-module.exports.stream = {
-    write: function(message, encoding){
-        logger.info(message);
-    }
-};
-
-
-
-
-//logger.log("info", "File: was found");
-logger.debug("This is a log");
+*/
 
 
 
