015613c5ef0604cc68e457a447b832f6e5400ba7
Matt Drapchaty <Drapchaty@comcast.net>
2016-11-05 20:23:08 -0600

    progressing through api.js

--- url-shortener/routes/api.js
+++ url-shortener/routes/api.js
@@ -1,51 +1,40 @@
-//claim module as function so that you can control what is returned
-module.exports = function(express){
-	var router = express.Router();  //connect to express
+// claim module as function so that you can control what is returned
+module.exports = function exp(express) {
+  const router = express.Router();  // connect to express
 
 
+// CONNECT TO DB
+  const Sequelize = require('sequelize');
 
-//   CONNECT TO DB
-	var Sequelize = require('sequelize')
-  , sequelize = new Sequelize('urldb', 'root', 'root', {
-      dialect: "mysql", // or 'sqlite', 'postgres', 'mariadb'
-      port:    3306, //..3306 or 5432 (for postgres)
-    });
-
-sequelize
-  .authenticate()
-  .then(function(err) {
-    console.log('Connection has been established successfully.');
-  }, function (err) { 
-    console.log('Unable to connect to the database:', err);
+  const sequelize = new Sequelize('urldb', 'root', 'root', {
+    dialect: 'mysql', // or 'sqlite', 'postgres', 'mariadb'
+    port: 3306, //..3306 or 5432 (for postgres)
   });
-
 
 // DEFINING A MODEL
-var Url = sequelize.define('urls', {
-  short_url: Sequelize.STRING,
-  long_url: Sequelize.STRING
-}, {
-  tableName: 'urls'
-});
-
-
-//SYNCHRONIZE SCHEMA
-sequelize
-  .sync({ force: false })
-  .then(function(err) {
-    console.log('It worked!');
-  }, function (err) { 
-    console.log('An error occurred while creating the table:', err);
+  const Url = sequelize.define('urls', {
+    short_url: Sequelize.STRING,
+    long_url: Sequelize.STRING,
+  }, {
+    tableName: 'urls',
   });
 
 
-  router.post('/v1/:url', function(req, res){  //post runs this function which is activated on this route /v1/:url  
-		var longUrl = req.params.url
-		
-				function makeid() // random 5 digit string generater
-		{
-		    var text = '';
-		    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  sequelize
+  .authenticate();
+
+
+// SYNCHRONIZE SCHEMA
+  sequelize
+  .sync({ force: false });
+
+
+  router.post('/v1/:url', function post(req, res) { // post runs this function which is activated on this route /v1/:url
+    const longUrl = req.params.url;
+
+    function makeid() { // random 5 digit string generater
+      let text = '';
+      const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
 		    for( var i=0; i < 5; i++ )
 		        text += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -75,7 +64,7 @@ sequelize
 
 
 	router.get('/v1/urls', function(req, res){   //Endpoint shows all urls, both original and shortened
-		
+
 			Url.findAll({
 	  attributes: ['short_url', 'long_url']
 		}).then(function (Url) {
@@ -90,7 +79,7 @@ sequelize
 
 
 	router.get('/v1/url/:id', function(req, res){  // Get a single row's short/original urls based on id entered
-		
+
 			Url.findAll({
 				where: {id: req.params.id},
 	  			attributes: ['short_url', 'long_url']
@@ -104,8 +93,8 @@ sequelize
 	});
 
 
-	router.delete('/v1/url/:id', function(req, res){ // delete row from urls based on id entered 
-		
+	router.delete('/v1/url/:id', function(req, res){ // delete row from urls based on id entered
+
 			Url.destroy({
 				where: {id: req.params.id}
 			}).then(function (Url) {
@@ -118,8 +107,8 @@ sequelize
 	});
 
 
-	router.post('/v1/url/:id', function(req, res){ // update short url based on id entered 
-		
+	router.post('/v1/url/:id', function(req, res){ // update short url based on id entered
+
 					function makeid() // random 5 digit string generater
 		{
 		    var text = '';
@@ -140,7 +129,7 @@ sequelize
 
 
 			Url.update(values, selector).then(function () {
-				   
+
 
 			});
 
@@ -153,13 +142,13 @@ sequelize
 	  			attributes: ['long_url']
 			}).then(function (Url) {
 				    res.redirect("http://" + Url.long_url);
-				    
+
 			}).error(function (err) {
 			    console.log("Error:" + err);
 			});
 	});
 
-	
+
 
 
 
